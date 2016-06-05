7b146900cfe7b67bfa6784ccbe0bd48d5e1b44c9
Jason <jason@loblein.com>
2016-06-05 23:58:41 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'static_pages/home'
+  get 'static_pages/help'
   # You can have the root of your site routed with "root"
   root 'application#hello'
 
