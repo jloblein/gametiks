52d7a196e0a6642d56c88f73a40f2a9f33aa4c1c
jloblein <jason@loblein.com>
2017-01-26 03:24:17 +0000

    first github commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   }
 
   # Validate the attached image is image/jpg, image/png, etc
-  #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
    
   # Geocoder gem
   geocoded_by :full_address
